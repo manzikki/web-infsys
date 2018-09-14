0c1b7fa3ff89f06affa86c64c0fc01f91f5a9631
db4144898 <manzikki@gmail.com>
2018-09-14 06:15:37 -0400

    added a web demo project

---
+++ demo-project/f1web.cpp
@@ -0,0 +1,49 @@
+// Copyright 2018 <niinimakim@webster.ac.th>
+#include <iostream>
+#include <string>
+#include <sstream>
+
+class factorial {
+    public:
+    factorial(unsigned long number = 0) {
+        mynum = number;
+        int cumul = number;
+        if (number <= 1) {
+           myf = 1;
+        } else {
+           for (int i=number-1; i > 1; i--) cumul = cumul * i;
+           myf = cumul;
+        }
+    }
+    unsigned long getf() {
+        return myf;
+    }
+    std::string speak() {
+         std::stringstream ss;
+         ss << "The factorial of ";
+         ss << mynum;
+         ss << " is ";
+         ss << myf;
+         return ss.str();
+    }    
+    private:
+        unsigned long myf;
+        unsigned long mynum;
+};
+
+int main(int argc, char *argv[])
+{
+    std::cout << "content-type: text/html" << std::endl << std::endl;
+    std::cout << "<h1>Greatest program ever. Calculates factorials.</h1>" << std::endl;
+    if (argc > 1) {
+        //convert the first parameter (string) into integer
+        std::stringstream firstparam(argv[1]);
+        int num = 0;
+        firstparam >> num;
+        factorial f1(num);
+        std::cout << f1.speak() << "\n";
+    } else {
+        std::cout << "You should call this program with a parameter. By web: programname?number\n";
+    }
+    return 0;
+}
