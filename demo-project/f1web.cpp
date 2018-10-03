d4f4af3c117f209f86a47faee8b10214fc9d0f96
db4144898 <manzikki@gmail.com>
2018-10-03 00:48:06 -0400

    cgi support

--- demo-project/f1web.cpp
+++ demo-project/f1web.cpp
@@ -2,6 +2,8 @@
 #include <iostream>
 #include <string>
 #include <sstream>
+#include <cstring>
+#include <stdlib.h>
 
 class factorial {
     public:
@@ -38,15 +40,26 @@ class factorial {
 int main(int argc, char *argv[]) {
     std::cout << "content-type: text/html" << std::endl << std::endl;
     std::cout << "<h1>Greatest program ever. Calculates factorials.</h1>" << std::endl;
+    // first possibility: query coded in a string
+    char *value = getenv("QUERY_STRING"); 
+    int num = 0;
+    if (value != NULL) {
+        char *pch = std::strstr(value,"=");
+        if (pch != NULL) {
+           num = atoi( pch+1);
+        }
+    }
+    // second: command line
     if (argc > 1) {
-        //convert the first parameter (string) into integer
         std::stringstream firstparam(argv[1]);
-        int num = 0;
         firstparam >> num;
+    }
+    // if either: num > 0
+    if (num > 0) {
         factorial f1(num);
         std::cout << f1.speak() << "\n";
     } else {
-        std::cout << "You should call this program with a parameter. By web: programname?number\n";
+        std::cout << "You should call this program with a parameter. By web: programname?number or  programname?f=number\n";
     }
     return 0;
 }
